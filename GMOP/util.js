d1800c92001d438634c43e73804bbf0d80f4c032
Chợ Tốt Pi <moonbix.bnb@gmail.com>
2025-11-15 15:31:51 +0700

    Update util.js

--- GMOP/util.js
+++ GMOP/util.js
@@ -1,1 +1,13 @@
+require("dotenv").config();
+const StellarSDK = require("@stellar/stellar-sdk");
 
+const server = new StellarSDK.Horizon.Server("https://api.testnet.minepi.com");
+const NETWORK = "Pi Testnet";
+
+module.exports = {
+  server,
+  NETWORK,
+  issuer: StellarSDK.Keypair.fromSecret(process.env.ISSUER_SECRET),
+  distributor: StellarSDK.Keypair.fromSecret(process.env.DISTRIBUTOR_SECRET),
+  GMOP: new StellarSDK.Asset("GMOP", StellarSDK.Keypair.fromSecret(process.env.ISSUER_SECRET).publicKey())
+};
